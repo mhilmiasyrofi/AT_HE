86abb91a9d1c7e1f392b0082a3ff53da3a509920
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2020-12-12 15:06:15 +0800

    modify ALP HE to load adversarail data

--- CIFAR-10/losses.py
+++ CIFAR-10/losses.py
@@ -4,6 +4,15 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 from he import HELoss
 
+cifar10_mean = (0.4914, 0.4822, 0.4465) # equals np.mean(train_set.train_data, axis=(0,1,2))/255
+cifar10_std = (0.2471, 0.2435, 0.2616) # equals np.std(train_set.train_data, axis=(0,1,2))/255
+
+mu = torch.tensor(cifar10_mean).view(3,1,1).cuda()
+std = torch.tensor(cifar10_std).view(3,1,1).cuda()
+
+def normalize(X):
+    return (X - mu)/std
+
 def trades_loss(model, x_natural, y, optimizer, step_size=0.003, epsilon=0.031,
                     perturb_steps=10, beta=1.0, distance='l_inf', loss = 'trades', 
                     m = None, s = None):
@@ -108,7 +117,7 @@ def pgd_loss(model, x_natural, y, optimizer, step_size=0.003, epsilon=0.031,
         raise RuntimeError('A error occurred')
     return loss
 
-def alp_loss(model, x_natural, y, optimizer, step_size=0.003, epsilon=0.031, perturb_steps=10,
+def alp_loss(model, x_natural, x_adv, y, y_adv, optimizer, step_size=0.003, epsilon=8./255., perturb_steps=10,
                 beta=1.0, distance='l_inf', loss = 'alp', m = None, s = None):
     # define KL-loss
     if loss == 'alp':
@@ -118,42 +127,19 @@ def alp_loss(model, x_natural, y, optimizer, step_size=0.003, epsilon=0.031, per
     else:
         raise RuntimeError('No exsiting current loss')
 
-    model.eval()
-    batch_size = len(x_natural)
-    # generate adversarial example
-    x_adv = x_natural.detach() + 0.001 * torch.randn(x_natural.shape).cuda().detach()
-    if distance == 'l_inf':
-        for _ in range(perturb_steps):
-            x_adv.requires_grad_()
-            with torch.enable_grad():
-                if loss == 'alp':
-                    loss_c = F.cross_entropy(model(x_adv), y)
-                elif loss == 'alp_he':
-                    loss_c = criterion_loss(model(x_adv), y)
-                else:
-                    raise RuntimeError('A error occurred')
-
-            grad = torch.autograd.grad(loss_c, [x_adv])[0]
-            x_adv = x_adv.detach() + step_size * torch.sign(grad.detach())
-            x_adv = torch.min(torch.max(x_adv, x_natural - epsilon), x_natural + epsilon)
-            x_adv = torch.clamp(x_adv, 0.0, 1.0)
-    else:
-        x_adv = torch.clamp(x_adv, 0.0, 1.0)
     model.train()
-
-    x_adv = Variable(torch.clamp(x_adv, 0.0, 1.0), requires_grad=False)
     # zero gradient
     optimizer.zero_grad()
     # calculate robust loss
-    logits = model(x_natural)
-    logits_adv = model(x_adv)
+    logits = model(normalize(x_natural))
+    logits_adv = model(normalize(x_adv))
     
     if loss=='alp':
         loss_robust = 0.5 * F.cross_entropy(logits, y) + 0.5 * F.cross_entropy(logits_adv, y)
         loss_alp = F.mse_loss(logits, logits_adv)
         loss = loss_robust + beta * loss_alp
     elif loss == 'alp_he':
-        loss_robust = 0.5 * criterion_loss(logits, y, cm = m) + 0.5 * criterion_loss(logits_adv, y, cm = m)
+        loss_robust = 0.5 * criterion_loss(logits, y, cm = m) + 0.5 * criterion_loss(logits_adv, y_adv, cm = m)
         loss_alp = F.mse_loss(logits, logits_adv)
         loss = loss_robust + beta * loss_alp
     else:
